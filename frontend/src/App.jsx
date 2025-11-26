8c7959c5e81656649854c9c652610c63f22b2ee5
Sanjan Kumar <sanjan95070@gmail.com>
2025-11-26 22:35:19 +0530

    Switch to HashRouter for Render deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // frontend/src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Editor from "./components/Editor";
